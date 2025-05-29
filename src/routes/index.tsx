8cb769dbd17f6876b4560ba7c8781f229c95f930
wiliromario <wiliromarioakukom@gmail.com>
2025-05-29 19:08:07 +0700

    feat: multiple select task

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -22,18 +22,23 @@ import {
   Title3,
   Button,
   TableCellActions,
+  type TableColumnDefinition,
+  createTableColumn,
+  useTableFeatures,
+  useTableSelection,
+  type TableRowId,
+  TableSelectionCell,
 } from '@fluentui/react-components';
 import Container from '../components/Container';
+import { useCallback, useEffect, useState } from 'react';
 
 export const Route = createFileRoute('/')({
   component: Index,
 });
 
-type ItemType = {
-  [key: string]: string | Date | string[];
-};
+type ItemType = Record<string, unknown>;
 
-const items: ItemType[] = [
+const itemsMock: ItemType[] = [
   {
     id: '0000000000000',
     organizationId: '00000000000',
@@ -136,60 +141,153 @@ const items: ItemType[] = [
   },
 ];
 
-const columns = [
-  { columnKey: 'title', label: 'Title', type: 'string', required: true },
+const columnsMock = [
+  { name: 'title', label: 'Title', type: 'string', required: true },
   {
-    columnKey: 'description',
+    name: 'description',
     label: 'Description',
     type: 'string',
   },
   {
-    columnKey: 'dueDate',
+    name: 'dueDate',
     label: 'Due Date',
     type: 'date',
   },
   {
-    columnKey: 'priority',
+    name: 'priority',
     label: 'Priority',
     type: 'select',
     options: ['low', 'medium', 'high'],
     required: true,
   },
   {
-    columnKey: 'status',
+    name: 'status',
     label: 'Status',
     type: 'select',
     options: ['todo', 'in-progress', 'completed'],
     required: true,
   },
-  { columnKey: 'tags', label: 'Tags', type: 'array' },
+  { name: 'tags', label: 'Tags', type: 'array' },
 ];
 
 function Index() {
+  const [taskSelected, setTaskSelected] = useState(0);
+  const [selectedRows, setSelectedRows] = useState(
+    () => new Set<TableRowId>([]),
+  );
+
+  useEffect(() => {
+    setTaskSelected(Array.from(selectedRows).length);
+  }, [selectedRows]);
+
+  const handleResetSelected = () => {
+    setSelectedRows(() => new Set<TableRowId>([]));
+  };
+
   const renderTitle = () => {
     return <Title3>List Task</Title3>;
   };
   const renderAction = () => {
+    if (taskSelected > 0) {
+      return (
+        <div>
+          <Button appearance="primary" onClick={handleResetSelected}>
+            Back
+          </Button>
+          <Button style={{ marginLeft: '10px' }}>
+            Delete {taskSelected} Task
+          </Button>
+        </div>
+      );
+    }
     return <Button appearance="primary">Add Task</Button>;
   };
+
+  const columns: TableColumnDefinition<ItemType>[] = columnsMock.map((item) => {
+    return createTableColumn<ItemType>({
+      columnId: item.name,
+    });
+  });
+
+  const {
+    getRows,
+    selection: {
+      allRowsSelected,
+      someRowsSelected,
+      toggleAllRows,
+      toggleRow,
+      isRowSelected,
+    },
+  } = useTableFeatures({ columns: columns, items: itemsMock }, [
+    useTableSelection({
+      selectionMode: 'multiselect',
+      selectedItems: selectedRows,
+      onSelectionChange: (e, data) => setSelectedRows(data.selectedItems),
+    }),
+  ]);
+
+  const rows = getRows((row) => {
+    const selected = isRowSelected(row.rowId);
+    return {
+      ...row,
+      onClick: (e: React.MouseEvent) => toggleRow(e, row.rowId),
+      onKeyDown: (e: React.KeyboardEvent) => {
+        if (e.key === ' ') {
+          e.preventDefault();
+          toggleRow(e, row.rowId);
+        }
+      },
+      selected,
+      appearance: selected ? ('brand' as const) : ('none' as const),
+    };
+  });
+
+  const toggleAllKeydown = useCallback(
+    (e: React.KeyboardEvent<HTMLDivElement>) => {
+      if (e.key === ' ') {
+        toggleAllRows(e);
+        e.preventDefault();
+      }
+    },
+    [toggleAllRows],
+  );
+
   return (
     <Container title={renderTitle()} action={renderAction()}>
       <Table arial-label="Default table" style={{ minWidth: '510px' }}>
         <TableHeader>
           <TableRow>
-            {columns.map((column) => (
-              <TableHeaderCell key={column.columnKey}>
+            <TableSelectionCell
+              checked={
+                allRowsSelected ? true : someRowsSelected ? 'mixed' : false
+              }
+              onClick={toggleAllRows}
+              onKeyDown={toggleAllKeydown}
+              checkboxIndicator={{ 'aria-label': 'Select all rows ' }}
+            />
+            {columnsMock.map((column) => (
+              <TableHeaderCell key={column.name}>
                 {column.label}
               </TableHeaderCell>
             ))}
           </TableRow>
         </TableHeader>
         <TableBody>
-          {items.map((item) => {
+          {rows.map(({ item, selected, onClick, onKeyDown, appearance }) => {
             return (
-              <TableRow key={item.id}>
-                {columns.map((column, index) => {
-                  const key: keyof typeof item = column.columnKey;
+              <TableRow
+                key={item?.id}
+                onClick={onClick}
+                onKeyDown={onKeyDown}
+                aria-selected={selected}
+                appearance={appearance}
+              >
+                <TableSelectionCell
+                  checked={selected}
+                  checkboxIndicator={{ 'aria-label': 'Select row' }}
+                />
+                {columnsMock.map((column, index) => {
+                  const key: keyof typeof item = column.name;
                   return (
                     <TableCell>
                       <TableCellLayout>{item[key]}</TableCellLayout>
