9d088ba111568ae9590ae518aa2ade59937b0ca5
wiliromario <wiliromarioakukom@gmail.com>
2025-05-31 14:15:37 +0700

    feat: hooks add new task and get task list

---
+++ src/hooks/useGetTasksList.ts
@@ -0,0 +1,18 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from '../api/axios';
+import { organizationId } from '../constants';
+
+type TaskType = Record<string, unknown>;
+
+const fetchTasks = async (): Promise<TaskType[]> => {
+  const response = await axios.get<TaskType[]>('/GetTasks', {params: {organizationId: organizationId}});
+  return response.data;
+};
+
+export const useGetTasksList = () => {
+  return useQuery({
+    queryKey: ['getTasks'],
+    queryFn: fetchTasks,
+    staleTime: 1000 * 60 * 5, // 5 minutes
+  });
+};
